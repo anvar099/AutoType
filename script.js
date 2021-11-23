d79e9a0cd58116d758b7a481d434c22713ffad9c
Anvar <63100534+anvar099@users.noreply.github.com>
2021-11-23 16:02:48 +0500

    done

---
+++ script.js
@@ -0,0 +1,19 @@
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+
+const text = "Subsribe"
+
+let ind = 1
+let speed = 300 / speedEl.value
+
+writeText()
+function writeText() {
+  textEl.innerText = text.slice(0, ind)
+
+  ind++
+  if(ind > text.length) {
+    ind = 1
+  }
+  setTimeout(writeText, speed)
+}
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
